2359728e22ccf3e2094423969a25527fe9c8c915
Philip Lundström <philip.lundstrom@live.se>
2024-12-12 21:29:43 +0100

    added some code

--- app/Day11.hs
+++ app/Day11.hs
@@ -5,25 +5,38 @@ import System.Environment
 import qualified Data.Map.Strict as Map
 import Data.Maybe (fromMaybe)
 
-type Level = Int
-type Expansion = [String]
+-- Tree structure to hold strings and their level information
+data Tree = Node 
+  { value :: String            -- Value of the string
+  , children :: [Tree]         -- Children nodes
+  , levelMap :: Map.Map Int [String] -- Map of levels to their values
+  } deriving (Show, Eq)
 
-type LevelExpansion = Map.Map Level Expansion
+-- Create a new node
+createNode :: String -> [Tree] -> Tree
+createNode val childs = Node val childs (buildLevelMap 0 childs)
 
-type ExpansionMap = Map.Map String LevelExpansion
--- Lookup in the nested map
-lookupInNestedMap :: String -> Level -> ExpansionMap -> Maybe Expansion
-lookupInNestedMap outerKey innerKey nestedMap =
-    case Map.lookup outerKey nestedMap of
-        Nothing -> Nothing
-        Just innerMap -> Map.lookup innerKey innerMap
+-- Helper to build the level map
+buildLevelMap :: Int -> [Tree] -> Map.Map Int [String]
+buildLevelMap level childs =
+  let currentLevel = Map.singleton level (map value childs)
+      childMaps = map (\child -> buildLevelMap (level + 1) (children child)) childs
+  in foldr (Map.unionWith (++)) currentLevel childMaps
 
+-- Apply rules to all strings in the tree
+applyRules :: [String -> String] -> Tree -> Tree
+applyRules rules (Node val childs _) =
+  let newValue = foldr (\rule acc -> rule acc) val rules
+      newChildren = map (applyRules rules) childs
+  in Node newValue newChildren (buildLevelMap 0 newChildren)
 
+-- Example rule: Add a prefix to strings
+addPrefixRule :: String -> String -> String
+addPrefixRule prefix str = prefix ++ str
 
-insertInsideNestedMap :: String -> Level -> ExpansionMap -> Expansion -> ExpansionMap
-insertInsideNestedMap str n emap expansion = Map.insert str (Map.singleton n expansion) emap
-
-
+-- Example rule: Convert string to uppercase
+uppercaseRule :: String -> String
+uppercaseRule = map toUpper
 part1 :: IO ()
 part1 = do
     initialStones <- wordParser "./inputs/test11.txt"
